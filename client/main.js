8a9b2e2d390f82442e8f3da3a58ec586cfda420e
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-10-03 20:52:02 +0200

    feat: add electron-packager with the npm 'package' script

--- client/main.js
+++ client/main.js
@@ -2,6 +2,8 @@
 
 const { app, BrowserWindow } = require('electron');
 
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
+
 function createWindow() {
   // Create the browser window.
   let win = new BrowserWindow({
