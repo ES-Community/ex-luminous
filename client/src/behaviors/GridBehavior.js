b0aaea6ebc155700861ed12a2a6bfa34ffe8a5cd
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-10-06 00:01:22 +0200

    feat: add new Grid generator

--- client/src/behaviors/GridBehavior.js
+++ client/src/behaviors/GridBehavior.js
@@ -1,12 +1,23 @@
 "use strict";
 
+// Require Third-party Dependencies
 const THREE = require("three");
 
+// Cube textures
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material2 = new THREE.MeshBasicMaterial({ color: new THREE.Color(30, 100, 50) });
+
 const Grid = {
+  cubeSize: 4,
+  defaultYPosition: 0,
+  createCubeMesh() {
+    const geometry = new THREE.BoxGeometry(this.cubeSize, this.cubeSize, this.cubeSize);
+
+    return new THREE.Mesh(geometry, material);
+  },
   generateGrid(sizeX, sizeY, scene) {
     const gridSize = sizeX * sizeY * 6.25;
-    var geometry = new THREE.BoxGeometry(4, 4, 4);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+    const geometry = new THREE.BoxGeometry(this.cubeSize, this.cubeSize, this.cubeSize);
 
     let initialX = -50;
     let initialZ = -50;
@@ -27,6 +38,19 @@ const Grid = {
       }
       initialZ = initialZ + 4;
       compteur++;
+    }
+  },
+  *generateGridEx(sizeX, sizeY) {
+    const geometry = new THREE.BoxGeometry(this.cubeSize, this.cubeSize, this.cubeSize);
+
+    for (let x = -sizeX / 2; x < sizeX / 2; x++) {
+      for (let y = -sizeY / 2; y < sizeY / 2; y++) {
+        const factor = Math.random() > 0.9;
+        const cube = new THREE.Mesh(geometry, factor ? material2 : material);
+        cube.position.set(x * this.cubeSize, this.defaultYPosition, y * this.cubeSize);
+
+        yield cube;
+      }
     }
   }
 };
