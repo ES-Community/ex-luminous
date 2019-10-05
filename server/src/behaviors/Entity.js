2fb7e5852f83fe2478347d7d042cd646e1e8c07c
Xavier Stouder <xavier@stouder.io>
2019-10-05 16:54:38 +0200

    fix: fix import

--- server/src/behaviors/Entity.js
+++ server/src/behaviors/Entity.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const getId = require("../utils/getId");
+const mandatory = require("../utils/mandatoryParam");
 
 class Entity {
   constructor(position = mandatory("position"), healthPoints = mandatory("healthPoints")) {
