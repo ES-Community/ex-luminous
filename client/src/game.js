a08060f156fb81ff54adc147c7d2ad6aae4618d7
Malaj Alexandre <alexandre.malaj@gmail.com>
2019-10-07 05:06:35 +0200

    accept conflict change

--- client/src/game.js
+++ client/src/game.js
@@ -217,11 +217,7 @@ async function initializeGameRenderer(gameDataStream, mapSize, playerName) {
   // Initialize Camera & Controls
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
   camera.name = "Camera";
-<<<<<<< HEAD
   camera.position.set(50, 120, 0);
-=======
-  camera.position.set(20, 20, 0);
->>>>>>> new orb design
   camera.lookAt(Player.threeObject.position);
   currentScene.add(camera);
   Player.threeObject.add(camera);
